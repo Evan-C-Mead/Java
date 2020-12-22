8a5b526e99949a8bcdf7f8a9a1218d1ecc0c6151
Evan Mead <evan.c.mead@gmail.com>
2020-12-22 15:48:29 -0600

    fizzbuzz example - took longer to create from memory than expected

---
+++ src/FizzBuzz.java
@@ -0,0 +1,22 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        fizzBuzz();
+    }
+
+    public static void fizzBuzz() {
+
+        for (int x = 1; x <= 100; x++) {
+            if (x % 15 == 0) {
+                System.out.printf("FizzBuzz %d\n", x);
+            } else if (x % 5 == 0) {
+                System.out.printf("Buzz %d\n", x);
+            } else if (x % 3 == 0) {
+                System.out.printf("Fizz %d\n", x);
+            } else {
+                System.out.printf("%d\n", x);
+            }
+        }
+
+    }
+}

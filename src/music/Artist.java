1a6f44b55caf39906ff244833a1ca9e84b1b1996
Evan Mead <evan.c.mead@gmail.com>
2021-01-03 19:57:08 -0600

    artist class created

---
+++ src/music/Artist.java
@@ -0,0 +1,18 @@
+package music;
+
+public class Artist {
+
+    private String artistName;
+
+    public Artist(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+}
